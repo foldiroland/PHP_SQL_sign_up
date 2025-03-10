0e04650cd57eb45b83f3f39a4655a55fa9fd89d5
Annus Kitti <145543117+foldiroland@users.noreply.github.com>
2025-03-10 09:20:36 +0100

    nemtudom mit csinaltam, annyira nem megy

--- feldolgoz_R.php
+++ feldolgoz_R.php
@@ -13,11 +13,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST'){
     $sql = "INSERT INTO felhasznalok (felhasznalonev, email, jelszo) VALUES ('" . $nev . "' , '" . $email . "', '" . $password . "')";
 }
 
-$result = $conn->query($sql);
+if ($conn->query($sql) === TRUE) {
+    $last_id = $conn->insert_id;
+    echo "New record created successfully. Last inserted ID is: " . $last_id;
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
 
-while ($row = $result->fetch_assoc()){
-    echo $row['name'] . "," . $row['email']."<br>";
-}
+
 
 
 ?>
